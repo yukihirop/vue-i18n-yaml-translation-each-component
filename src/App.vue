0c910f0c56ab980116ca32ac8064643530bed03f
yukihirop <te108186@gmail.com>
2021-07-18 13:42:38 +0900

    Add changeLocale

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,25 @@
     <router-link to="/">{{ $t("link.home") }}</router-link> |
     <router-link to="/about">{{ $t("link.about") }}</router-link>
   </div>
+  <button @click="changeLocale">Change Locale</button>
   <router-view />
 </template>
+
+<script lang="ts">
+import { getCurrentInstance, onMounted } from "vue";
+export default {
+  setup() {
+    const vm = getCurrentInstance()!.proxy!;
+    
+    function changeLocale(){
+      vm.$i18n.locale = (vm.$i18n.locale === 'ja') ? 'en' : 'ja';
+    }
+    return {
+      changeLocale
+    };
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
